67da9d98a6015d7a313d113794d647fbdc38fc82
delinak <delina.kiflom14@gmail.com>
2025-03-08 13:01:54 -0500

    setup

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from "./ui/button";
+
+function Header() {
+  return (
+    <div>
+        <Button>
+            Click Me
+        </Button>
+    </div>
+  )
+}
+
+export default Header
